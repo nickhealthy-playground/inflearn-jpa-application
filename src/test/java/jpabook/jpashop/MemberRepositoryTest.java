3cd0050fcfd4709f19b9c5562d92007a4cbe9aab
nick <alskadmlcraz1@gmail.com>
2026-04-09 17:05:47 +0900

    JPA와 DB 설정, 동작확인

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @Transactional
+    void save() {
+        Member member = new Member();
+        member.setName("memberA");
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+        assertThat(findMember).isEqualTo(member);
+
+        em.flush();
+        em.clear();
+    }
+
+}
